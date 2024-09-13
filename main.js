07223b8928cc5176ee534ddab8e4fbbc4a84cfd9
aklimaakter20 <aklimaakter20@gmail.com>
2024-09-13 13:29:29 +0600

    javascript math uploaded

---
+++ main.js
@@ -0,0 +1,75 @@
+// 1 no Ans
+
+let saraHave = 25;
+let saraBought = 15;
+let totalApples = saraHave + saraBought;
+let finalApples = "Total Number of Apples : " + " " + totalApples ;
+console.log(finalApples);
+
+// 2 no Ans
+
+let johnHad = 500;
+let johnSpent = 200;
+let johnHave = johnHad - johnSpent;
+let johnHaveNow = "Now John Has : " + " " + johnHave + " " + "Taka";
+console.log(johnHaveNow);
+
+// 3 no Ans
+
+let perHour = 120;
+let totalHours = 8;
+let totalMachine = perHour * totalHours;
+let totalCreateMachine = "The machine will make : " + " " + totalMachine;
+console.log(totalCreateMachine);
+
+// 4 no Ans
+
+let treeHave = 12;
+let rowHave = 8;
+let totalTree = treeHave * rowHave;
+let planOfTree = 20;
+let totalNumberOfTree = totalTree + planOfTree;
+let totalTreeHave = "Total Tree Have : " + totalNumberOfTree;
+console.log(totalTreeHave);
+
+// 5 no Ans
+
+let boxHave = 5;
+let orangeHave = 10;
+let totalOrange = boxHave * orangeHave;
+let friendGave = 12;
+let totalOrangeNow = totalOrange - friendGave;
+let totalNumberOfOrange = "Total Number Of Orange : " + " "
++ totalOrangeNow;
+console.log(totalNumberOfOrange);
+
+// 6 no Ans
+
+let trainCrosses = 150
+let totalCrossesHours = 3
+let perCrossesHour = trainCrosses / totalCrossesHours;
+let perHours = "The train passes every hour : " + " " + perCrossesHour;
+console.log(perHours); 
+
+// 7 no Ans
+
+let productMake = 200;
+let useMachine = 6;
+let perMachineMake = productMake /useMachine;
+let addMachine = 2;
+let totalMachines = useMachine + addMachine; 
+let totalProductMake = perMachineMake * totalMachines;
+let notSellProduct = 3;
+let remainingProduct = totalProductMake - notSellProduct;
+let remainingProductNow = "Remaining product : " + " " + remainingProduct;
+
+console.log(remainingProductNow);
+
+
+// 8 no Ans
+
+let chocolateBought = 120;
+let KeepsChocolate = 9;
+let remainingChocolate = chocolateBought % KeepsChocolate;
+let totalRemainingChocolate = "Total Chocolate Remaining : " + " " + remainingChocolate;
+console.log(totalRemainingChocolate);
